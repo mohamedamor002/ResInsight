8512e3eecdce40d2a93945974ddbb7de3c50b9b8
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-08-08 11:20:47 +0100

    style: Format

--- app.py
+++ app.py
@@ -6,37 +6,39 @@ import pdf_extraction
 app = Flask(__name__)
 turbo = Turbo(app)
 
-@app.get('/')
+
+@app.get("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 UNPROCESSABLE_ENTITY = 422
+
 
 def form_error(template: str, **context):
     return render_template(template, **context), UNPROCESSABLE_ENTITY
 
 
-@app.post('/upload')
+@app.post("/upload")
 def upload_file():
 
     upload_error: str | None = None
 
-    if 'file' not in request.files:
+    if "file" not in request.files:
         upload_error = "No file part."
-        return form_error('index.html', upload_error=upload_error)
+        return form_error("index.html", upload_error=upload_error)
 
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         upload_error = "No uploaded file."
-        return form_error('index.html', upload_error=upload_error)
+        return form_error("index.html", upload_error=upload_error)
 
-    if file.content_type != 'application/pdf':
+    if file.content_type != "application/pdf":
         upload_error = "File must be a PDF."
-        return form_error('index.html', upload_error=upload_error)
+        return form_error("index.html", upload_error=upload_error)
 
     text = pdf_extraction.pdf_to_text(file)
-    insights  = parsing_model.parse_resume(text)
+    insights = parsing_model.parse_resume(text)
     if turbo.can_stream():
         return turbo.stream(
             (
@@ -46,10 +48,8 @@ def upload_file():
                 turbo.update("", "error"),
             )
         )
-    return render_template(
-        "index.html",
-        insights=insights
-    )
+    return render_template("index.html", insights=insights)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
