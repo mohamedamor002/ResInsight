445022d46f4eb61d6c992e0b4586fda1392975e0
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-08-08 11:50:08 +0100

    refactor: Remove useless parentheses

--- pdf_extraction.py
+++ pdf_extraction.py
@@ -6,4 +6,4 @@ import fitz
 def pdf_to_text(file: FileStorage) -> str:
     buffer = BytesIO(file.stream.read())
     with fitz.open(stream=buffer, filetype="pdf") as pdf:
-        return "\n".join((page.get_text() for page in pdf.pages()))
+        return "\n".join(page.get_text() for page in pdf.pages())
